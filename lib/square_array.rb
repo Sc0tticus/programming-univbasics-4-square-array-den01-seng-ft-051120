ec85117783b7022891696022b804f8bc7ff467ee
Scott Singer <ssinger303@gmail.com>
2020-04-30 21:17:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,12 @@
+#array=[1,2,3,4]
+#new_array=[]
+#array.length.times do |index|
+# new_array.push(array[index]**2)
+#end
+#new_array=[1,4,0,16]
+
+
+
 def square_array(array)
-  # your code here
-  counter = 0 
-  
-  while counter < array.length do
-    puts array[counter]**2 
-    counter += 1 
-  end
+
 end
